08b95cfe9ae744f9fedf4b297ea9d9de2981ddb3
Bong <borobong230@gmail.com>
2025-04-12 21:03:40 +0900

    :sparkles: [Feat] meta, links 분리 #main

--- app/potato/tomato.tsx
+++ app/potato/tomato.tsx
@@ -1,3 +1,7 @@
 export default function AboutUs() {
   return <div>About Us</div>;
 }
+
+export const links = () => [{ rel: "stylesheet", href: "tomato.com" }];
+
+export const meta = () => [{ title: "About Us" }];
