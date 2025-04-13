14ec68269b412a13ca8ba2a859d13bf9bba91e2b
Bong <borobong230@gmail.com>
2025-04-13 23:12:38 +0900

    :fire: [Remove]  불필요한 파일 제거 #main

--- app/potato/tomato.tsx
+++
@@ -1,7 +0,0 @@
-export default function AboutUs() {
-  return <div>About Us</div>;
-}
-
-export const links = () => [{ rel: "stylesheet", href: "tomato.com" }];
-
-export const meta = () => [{ title: "About Us" }];
